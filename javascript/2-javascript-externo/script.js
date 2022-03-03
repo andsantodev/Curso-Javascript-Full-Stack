88f86d0d6de1e9a95776c5903bb1269b7177ab5a
Anderson <and.santos123@gmail.com>
2022-03-02 23:46:48 -0300

    realocando pastas

---
+++ javascript/2-javascript-externo/script.js
@@ -0,0 +1,9 @@
+alert("Diferença de idade entre duas pessoas.");
+let nomePessoaVelha = prompt("Nome da pessoa mais velha?");
+let idadePessoaVelha = prompt("Idade da pessoa mais velha?");
+let nomePessoaNova = prompt("Nome da pessoa mais nova?");
+let idadePessoaNova = prompt("Idade da pessoa mais nova?");
+let diferenca = idadePessoaVelha - idadePessoaNova;
+alert(`A pessoa mais velha se chama ${nomePessoaVelha} e tem ${idadePessoaVelha} anos
+A pessoa mais nova se chama ${nomePessoaNova} e tem ${idadePessoaNova} anos
+deferença entre as duas idades é de ${diferenca} anos.`);
